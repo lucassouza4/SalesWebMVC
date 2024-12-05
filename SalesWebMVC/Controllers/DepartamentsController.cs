fa5d8f77d025d3cf249a9ffdbdeee67cce7514db
lucassouza4 <lussilva1998@gmail.com>
2024-12-05 03:11:53 -0300

    MVC Departament

---
+++ SalesWebMVC/Controllers/DepartamentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>();
+            departments.Add(new Department{ Id=1,Name="Eletronics"});
+            departments.Add(new Department { Id=2,Name="Fashion"});
+            return View(departments);
+        }
+    }
+}
