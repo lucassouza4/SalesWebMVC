b2a3721a6bfad86c510e115b9c396070335fdf16
lucassouza4 <lussilva1998@gmail.com>
2024-12-06 14:11:34 -0300

    Outras entidades e migrations

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,8 +1,21 @@
-﻿namespace SalesWebMVC.Models
+﻿using System.Linq;
+using Humanizer;
+
+namespace SalesWebMVC.Models
 {
-    public class Department
+    public class Department(int id, string name)
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; set; } = id;
+        public string Name { get; set; } = name;
+        public ICollection<Seller> Sellers { get; set; } = [];
+
+        public void AddSeller(Seller seller) { 
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
